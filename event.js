c31f79463217638b67301205cc6b4000e9a4eb73
pradeepa <rajapradeepar@gmail.com>
2020-10-13 14:12:59 +0530

    upload

---
+++ event.js
@@ -0,0 +1,14 @@
+(function(){
+
+    'use strict';
+
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope) {
+    $scope.showMe = false;
+    $scope.myFunc = function() {
+        $scope.showMe = !$scope.showMe;
+
+    }
+});
+
+})();
---
+++ ngrepeat.js
@@ -0,0 +1,25 @@
+(function(){
+
+    'use strict';
+
+
+
+var app = angular.module('DemoApp',[]);
+	app.controller('DemoController', function($scope){
+
+	$scope.TopicNames =[
+		{name: "1.Kodaikanal lake is an artificial, roughly star-shaped 45-hectare (110-acre) lake built in 1863. It is Kodaikanal's most popular geographic landmark and tourist attraction."},
+		{name: "2.Bryant Park: The park was planned and built in 1908 by a forest officer from Madurai, H.D. Bryant, and named after him. With 325 species of trees, shrubs, and cacti, the park is a rainbow of flowers during the peak season."},
+		{name: "3.Coaker's Walk, 500 metres (1,600 ft) from the bus-stand, constructed by Lt. Coaker in 1872, is a 1-kilometre (3,300 ft) paved pedestrian path running along the edge of steep slopes on the southern side of Kodai."},
+		{name: "4.Poombarai Village (Kudhanthai Velappar Temple) is 15 kilometres (9.3 mi) from the bus-stand. In Poombarai village there is a temple of Lord Muruga. The village is fully covered by reserve forest. The final approach to this quiet area is a gently climbing foot-path"},
+		{name: "5.Pine forests: In 1906, with a view to growing valuable timber, H.D. Bryant started the Kodaikanal pine plantations in the south-west of Kodaikanal"},
+		{name: "6.Green Valley View (formerly called Suicide Point), 5.5 kilometres (3.4 mi) from the bus-stand and near the golf course, has a panoramic view of the plains and a sheer drop of 1,500 metres (4,900 ft) overlooking the Vaigai Dam to the south. "},
+		{name: "7.Shembaganur Museum of Natural History, 6 kilometres (3.7 mi) from the bus-stand, founded in 1895, is open to the public (except Tuesdays) for viewing their outstanding taxidermy collection of more than 500 species of animals, birds and insects and a living collection of over 300 exotic orchid species."},
+		{name: "8.Guna caves made popular by the Tamil movie Gunaa, previously called Devil's Kitchen, are deep bat-infested chambers between the three gigantic boulders that are the Pillar Rocks. The deep narrow ravines of the caves are now closed to public due to the deaths of twelve youths there."},
+		{name: "9.Pillar Rocks, 8 kilometres (5.0 mi) from the bus-stand, is a set of three giant rock pillars which stand 122 metres (400 ft) high.[35] Managed by the Tamil Nadu Forest Department, The viewpoint can be crowded but is not commercialized. There is an excellent public garden adjacent to the viewpoint."},
+		{name: "10.Silver Cascade, 8 kilometres (5.0 mi) from Kodaikanal at a wide bend in the long and winding Laws Ghat Road, at altitude 1,800 metres (5,900 ft), is a 55-metre (180 ft) waterfall formed from the outflow of Kodaikanal Lake.."}];	
+		});
+		
+		
+})();
+
